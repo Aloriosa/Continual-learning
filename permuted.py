a67bb9d887b58157dfb8e7c6b1d1eed1737556b9
Saraharas <34598622+Saraharas@users.noreply.github.com>
2019-10-25 17:47:49 +0300

    permuted MNIST generator class

---
+++ permuted.py
@@ -0,0 +1,61 @@
+class PermutedMnistGenerator():
+    def __init__(self, max_iter=10, random_seed=0):
+        # Open data file
+        f = gzip.open('/content/drive/My Drive/Colab Notebooks/mnist.pkl.gz', 'rb')
+        train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
+        f.close()
+
+        # Define train and test data
+        self.X_train = np.vstack((train_set[0], valid_set[0]))
+        self.Y_train = np.hstack((train_set[1], valid_set[1]))
+        self.X_test = test_set[0]
+        self.Y_test = test_set[1]
+        self.random_seed = random_seed
+        self.max_iter = max_iter
+        self.cur_iter = 0
+
+        self.out_dim = 10           # Total number of unique classes
+        self.class_list = range(10) # List of unique classes being considered, in the order they appear
+
+        # self.classes is the classes (with correct indices for training/testing) of interest at each task_id
+        self.classes = []
+        for iter in range(self.max_iter):
+            self.classes.append(range(0,10))
+
+        self.sets = self.classes
+
+    def get_dims(self):
+        # Get data input and output dimensions
+        return self.X_train.shape[1], self.out_dim
+
+    def next_task(self):
+        if self.cur_iter >= self.max_iter:
+            raise Exception('Number of tasks exceeded!')
+        else:
+            np.random.seed(self.cur_iter+self.random_seed)
+            perm_inds = list(range(self.X_train.shape[1]))
+            # First task is (unpermuted) MNIST, subsequent tasks are random permutations of pixels
+            if self.cur_iter > 0:
+                np.random.shuffle(perm_inds)
+
+            # Retrieve train data
+            next_x_train = deepcopy(self.X_train)
+            next_x_train = next_x_train[:,perm_inds]
+
+            # Initialise next_y_train to zeros, then change relevant entries to ones, and then stack
+            #next_y_train = np.zeros((len(next_x_train), 10))
+            #next_y_train[:,0:10] = np.eye(10)[self.Y_train]
+            next_y_train = deepcopy(self.Y_train)
+            # Retrieve test data
+            next_x_test = deepcopy(self.X_test)
+            next_x_test = next_x_test[:,perm_inds]
+
+            #next_y_test = np.zeros((len(next_x_test), 10))
+            #next_y_test[:,0:10] = np.eye(10)[self.Y_test]
+            next_y_test = deepcopy(self.Y_test)
+            self.cur_iter += 1
+
+            return next_x_train, next_y_train, next_x_test, next_y_test
+
+    def reset(self):
+        self.cur_iter = 0
